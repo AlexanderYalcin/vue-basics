332d706aa4fe011afb2faa9171f2fe74c6accc03
Alexander Yalcin <alexander.yalcin@appliedtechnology.se>
2021-02-25 19:37:23 +0100

    second

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
       name: '',
     };
   },
   methods: {
+    resetInput() {
+      this.name = '';
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('test');
+    },
     add(num) {
       this.counter = this.counter + num;
     },
@@ -13,7 +22,7 @@ const app = Vue.createApp({
       this.counter = this.counter - num;
     },
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
   },
 });
